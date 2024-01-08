f415071ae4d9e69ca91b5f2484001790abc85a08
priyaa1255 <125294288+priyaa1255@users.noreply.github.com>
2024-01-08 16:41:43 +0530

    Delete terraform directory

--- terraform/output.tf
+++
@@ -1,9 +0,0 @@
-# this file is used to print vpc-id and subnet-id, we will use them in firewall and server file
-
-output "vpc_id" {
-  value = aws_vpc.lms_vpc.id
-}
-
-output "subnet_id" {
-  value = aws_subnet.lms_public_subnet.id
-}
--- terraform/provider.tf
+++
@@ -1,6 +0,0 @@
-# AWS provider
-provider "aws" {
-  region     = "us-west-1"
-  access_key = "**"                        # your access key details
-  secret_key = "****"        # your secret key details
-}
